590e4fe9d2b9864a5aa9d0e24a3f94e1cb4946fa
peteris-racinskis <42273460+peteris-racinskis@users.noreply.github.com>
2021-05-17 14:10:59 +0300

    Update nmux4.v

--- MMU/nmux4.v
+++ MMU/nmux4.v
@@ -1,23 +1,5 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date:    15:57:37 05/12/2021 
-// Design Name: 
-// Module Name:    nmux4 
-// Project Name: 
-// Target Devices: 
-// Tool versions: 
-// Description: 
-//
-// Dependencies: 
-//
-// Revision: 
-// Revision 0.01 - File Created
-// Additional Comments: 
-//
-//////////////////////////////////////////////////////////////////////////////////
+
 module nmux4(a,b,c,d,sel,out,rdy);
 	
 	parameter WORDSIZE = 16;
